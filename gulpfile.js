7e1e8b4651c1816493a53582d83ac54728d1db8b
Isaac Salcedo <isalcedo16@aol.com>
2023-07-10 14:18:34 -0400

    Initial Commit

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+const { src, dest, watch } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+
+function css(done) {
+  src("src/scss/**/*.scss").pipe(sass()).pipe(dest("build/css"));
+  done();
+}
+
+function dev(done) {
+  watch("src/scss/**/*.scss", css);
+  done();
+}
+
+exports.css = css;
+exports.dev = dev;
