2ac0a2de1fcdd1580522e32bb78a80da43aa0e0f
Isaac Salcedo <isalcedo16@aol.com>
2023-07-25 20:16:09 -0400

    Creating webp images

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
-const { src, dest, watch } = require("gulp");
+const { src, dest, watch, parallel } = require("gulp");
+
+// CSS
 const sass = require("gulp-sass")(require("sass"));
 const plumber = require("gulp-plumber");
+
+// Imagenes
+const webp = require("gulp-webp");
 
 function css(done) {
   src("src/scss/**/*.scss")
@@ -10,10 +15,19 @@ function css(done) {
   done();
 }
 
+function versionwebp(done) {
+  const opciones = {
+    quality: 50,
+  };
+  src("src/img/**/*.{png,jpg}").pipe(webp(opciones)).pipe(dest("build/img"));
+  done();
+}
+
 function dev(done) {
   watch("src/scss/**/*.scss", css);
   done();
 }
 
 exports.css = css;
-exports.dev = dev;
+exports.versionwebp = versionwebp;
+exports.dev = parallel(versionwebp, dev);
